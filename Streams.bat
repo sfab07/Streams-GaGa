a83bdde3faa7dbcc684e9fa2063c88567c6b8858
sfab07 <sfab07@gmail.com>
2019-01-25 17:11:45 +0100

    Update Streams.bat

--- Streams.bat
+++ Streams.bat
@@ -1,1 +1,1 @@
-cd C:\Program Files (x86)\Radio gaga\ && curl https://raw.githubusercontent.com/sfab07/Streams.ini-GaGa/master/Streams.ini --output Streams.ini
+cd C:\Program Files (x86)\GaGa\ && curl https://raw.githubusercontent.com/sfab07/Streams.ini-GaGa/master/Streams.ini --output Streams.ini
